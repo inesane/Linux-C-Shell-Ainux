e7b97ccbce1801c6af7aaa30ff433b735a54a83d
ainesh <sannidhiainu@gmail.com>
2020-09-02 18:40:45 +0530

    added tokenize

--- main.c
+++ main.c
@@ -7,11 +7,19 @@ int main()
     while (1)
     {
         prompt();
-        char *buffer;
-        size_t bufsize = 32;
-        size_t characters;
-        characters = getline(&buffer, &bufsize, stdin);
-        if (strcmp(buffer, "pwd\n")==0)
+        char *buffer = NULL;
+        size_t bufsize = 0;
+        getline(&buffer, &bufsize, stdin);
+        char *inputs[100];
+        int args = 0;
+        inputs[args] = strtok(buffer, " ");
+        while (inputs[args] != NULL)
+        {
+            args++;
+            inputs[args] = strtok(NULL, " ");
+        }
+        inputs[args] = NULL;
+        if (strcmp(inputs, "pwd\n") == 0)
         {
             pwd();
         }
