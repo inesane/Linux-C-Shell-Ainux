0791d95527c153833b1bf31c289c5b4ad1b3a72d
ainesh <sannidhiainu@gmail.com>
2020-10-02 04:10:14 +0530

    Added fg bg

--- bg.c
+++ bg.c
@@ -3,12 +3,40 @@
 
 void bg(char *inputs[], int args)
 {
-    if(args!=2)
+    if (args != 2)
     {
         fprintf(stderr, "ERROR: Incorrect number of arguments\n");
     }
     else
     {
-        
+        int jobno = atoi(inputs[1]);
+        struct Node *copy = ll;
+        copy = copy->next;
+        struct Node *previous = ll;
+        int counter = 0;
+        while (copy != NULL)
+        {
+            counter++;
+            previous = copy;
+            copy = copy->next;
+        }
+        if(jobno>counter||jobno<=0)
+        {
+            fprintf(stderr, "ERROR: Invalid job number\n");
+            return;
+        }
+        copy = ll->next;
+        previous = ll;
+        while (copy != NULL)
+        {
+            if (counter == jobno)
+            {
+                break;
+            }
+            counter--;
+            previous = copy;
+            copy = copy->next;
+        }
+        kill(copy->data, SIGCONT);
     }
 }
--- fg.c
+++ fg.c
@@ -3,13 +3,59 @@
 
 void fg(char *inputs[], int args)
 {
-    if(args!=2)
+    if (args != 2)
     {
         fprintf(stderr, "ERROR: Incorrect number of arguments\n");
     }
     else
     {
-        
+        int jobno = atoi(inputs[1]);
+        struct Node *copy = ll;
+        copy = copy->next;
+        struct Node *previous = ll;
+        int counter = 0;
+        while (copy != NULL)
+        {
+            counter++;
+            previous = copy;
+            copy = copy->next;
+        }
+        if (jobno > counter || jobno <= 0)
+        {
+            fprintf(stderr, "ERROR: Invalid job number\n");
+            return;
+        }
+        copy = ll->next;
+        previous = ll;
+        while (copy != NULL)
+        {
+            if (counter == jobno)
+            {
+                break;
+            }
+            counter--;
+            previous = copy;
+            copy = copy->next;
+        }
+        int stat;
+        signal(SIGTTOU, SIG_IGN);
+        signal(SIGTTIN, SIG_IGN);
+        tcsetpgrp(STDIN_FILENO, copy->data);
+        kill(copy->data, SIGCONT);
+        waitpid(copy->data, &stat, WUNTRACED);
+        tcsetpgrp(STDIN_FILENO, getpgrp());
+        signal(SIGTTOU, SIG_DFL);
+        signal(SIGTTIN, SIG_DFL);
+        if (WIFSTOPPED(stat))
+        {
+            struct Node *copy2 = ll;
+            while (copy2->next != NULL)
+            {
+                copy2 = copy2->next;
+            }
+            struct Node *temp2;
+            copy2->next = copy;
+            copy->next=NULL;
+        }
     }
-    
 }
